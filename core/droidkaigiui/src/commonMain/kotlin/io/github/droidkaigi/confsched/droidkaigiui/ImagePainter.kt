9914878a3a13ab50377e329af8b14c8c92abd223
hanminjae <hanminjae131@gmail.com>
2025-08-23 02:33:47 +0900

    feat(droidkaigiui): Improve AsyncImage to handle loading and error states
    Modify rememberAsyncImagePainter to provide visual feedback during different image loading states.
    
    - On loading: Display a placeholder painter.
    - On error: Display a predefined error mascot image.
    - On success: Display the loaded image.
    
    This change improves user experience by clearly indicating the status of image loading.
    
    Fixes #159

--- core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/ImagePainter.kt
+++ core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/ImagePainter.kt
@@ -1,6 +1,8 @@
 package io.github.droidkaigi.confsched.droidkaigiui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import org.jetbrains.compose.resources.painterResource
 import coil3.compose.rememberAsyncImagePainter as coilRememberAsyncImagePainter
@@ -8,7 +10,7 @@ import coil3.compose.rememberAsyncImagePainter as coilRememberAsyncImagePainter
 @Composable
 fun rememberAsyncImagePainter(
     model: Any?,
-    placeholder: Painter = painterResource(DroidkaigiuiRes.drawable.error_mascot),
+    placeholder: Painter = ColorPainter(Color.Gray),
     error: Painter = painterResource(DroidkaigiuiRes.drawable.error_mascot),
 ): Painter {
     return coilRememberAsyncImagePainter(
