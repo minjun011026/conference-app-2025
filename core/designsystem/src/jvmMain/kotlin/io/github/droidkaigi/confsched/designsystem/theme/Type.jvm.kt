d3e5dd093b40aa99c8cddabfc653b62216363810
todayama_r <13657682+Corvus400@users.noreply.github.com>
2025-08-22 14:35:47 +0900

    :wrench: Use Compose Resources for fonts on JVM
    This commit updates the font loading mechanism on the JVM platform to use the `Font` function from Compose Resources.
    
    The following changes were made:
    - Removed the custom `fontFamilyFor` and `resolveTypeface` functions.
    - Updated `changoFontFamily`, `robotoRegularFontFamily`, and `robotoMediumFontFamily` to directly use `Font` with the appropriate resource identifiers and `FontWeight`.

--- core/designsystem/src/jvmMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.jvm.kt
+++ core/designsystem/src/jvmMain/kotlin/io/github/droidkaigi/confsched/designsystem/theme/Type.jvm.kt
@@ -2,53 +2,24 @@ package io.github.droidkaigi.confsched.designsystem.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.platform.Typeface
-import org.jetbrains.skia.FontMgr
-import org.jetbrains.skia.FontStyle
-import org.jetbrains.skia.Typeface
+import androidx.compose.ui.text.font.FontWeight
+import io.github.droidkaigi.confsched.designsystem.Chango_Regular
+import io.github.droidkaigi.confsched.designsystem.DesignsystemRes
+import io.github.droidkaigi.confsched.designsystem.Roboto_Medium
+import io.github.droidkaigi.confsched.designsystem.Roboto_Regular
+import org.jetbrains.compose.resources.Font
 
 @Composable
-actual fun changoFontFamily(): FontFamily = fontFamilyFor(
-    familyName = "Chango",
-    fontWeight = 400,
+actual fun changoFontFamily(): FontFamily = FontFamily(
+    Font(DesignsystemRes.font.Chango_Regular, FontWeight.Normal),
 )
 
 @Composable
-actual fun robotoRegularFontFamily(): FontFamily = fontFamilyFor(
-    familyName = "Roboto",
-    fontWeight = 500,
+actual fun robotoRegularFontFamily(): FontFamily = FontFamily(
+    Font(DesignsystemRes.font.Roboto_Regular, FontWeight.Normal),
 )
 
 @Composable
-actual fun robotoMediumFontFamily(): FontFamily = fontFamilyFor(
-    familyName = "Roboto",
-    fontWeight = 500,
+actual fun robotoMediumFontFamily(): FontFamily = FontFamily(
+    Font(DesignsystemRes.font.Roboto_Medium, FontWeight.Medium),
 )
-
-private fun fontFamilyFor(
-    familyName: String,
-    fontWeight: Int = 400,
-): FontFamily {
-    val skTypeface = resolveTypeface(
-        familyName = familyName,
-        fontWeight = fontWeight,
-    )
-    return try {
-        FontFamily(Typeface(skTypeface))
-    } finally {
-        skTypeface.close()
-    }
-}
-
-private fun resolveTypeface(
-    familyName: String,
-    fontWeight: Int = 400,
-): Typeface {
-    val normal = FontStyle.NORMAL
-    val style = FontStyle(fontWeight, normal.width, normal.slant)
-    val fontMgr = FontMgr.default
-
-    return fontMgr.matchFamilyStyle(familyName, style)
-        ?: fontMgr.matchFamilyStyle(null, style) // default system font.
-        ?: Typeface.makeEmpty()
-}
