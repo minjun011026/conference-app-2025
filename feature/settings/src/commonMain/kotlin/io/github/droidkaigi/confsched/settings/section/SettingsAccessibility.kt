05fa5a6ea1912852e0b7eca776ea8c55fab6aefc
todayama_r <13657682+Corvus400@users.noreply.github.com>
2025-08-24 02:05:24 +0900

    :wrench: Deleted unnecessary columns and modified to handle text only. https://github.com/DroidKaigi/conference-app-2025/pull/288#discussion_r2296173476

--- feature/settings/src/commonMain/kotlin/io/github/droidkaigi/confsched/settings/section/SettingsAccessibility.kt
+++ feature/settings/src/commonMain/kotlin/io/github/droidkaigi/confsched/settings/section/SettingsAccessibility.kt
@@ -1,12 +1,18 @@
 package io.github.droidkaigi.confsched.settings.section
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched.designsystem.theme.changoFontFamily
 import io.github.droidkaigi.confsched.designsystem.theme.robotoMediumFontFamily
@@ -14,7 +20,6 @@ import io.github.droidkaigi.confsched.designsystem.theme.robotoRegularFontFamily
 import io.github.droidkaigi.confsched.model.settings.KaigiFontFamily
 import io.github.droidkaigi.confsched.settings.SettingsRes
 import io.github.droidkaigi.confsched.settings.SettingsUiState
-import io.github.droidkaigi.confsched.settings.component.SelectableItemColumn
 import io.github.droidkaigi.confsched.settings.component.SettingsItemRow
 import io.github.droidkaigi.confsched.settings.ic_brand_family
 import io.github.droidkaigi.confsched.settings.section_item_title_font
@@ -51,13 +56,18 @@ fun LazyListScope.accessibility(
                         KaigiFontFamily.RobotoMedium -> robotoMediumFontFamily()
                         KaigiFontFamily.SystemDefault -> FontFamily.Default
                     }
-                    SelectableItemColumn(
+                    Text(
                         // TODO Add TestTag
+                        modifier = Modifier
+                            .clickable { onSelectUseFontFamily(fontFamily) }
+                            .fillMaxWidth()
+                            .height(72.dp)
+                            .padding(start = 48.dp)
+                            .wrapContentHeight(Alignment.CenterVertically),
+                        text = fontFamily.displayName,
                         fontFamily = itemFont,
-                        currentValue = fontFamily.displayName,
-                        onClickItem = {
-                            onSelectUseFontFamily(fontFamily)
-                        },
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.bodyLarge,
                     )
                 }
             },
