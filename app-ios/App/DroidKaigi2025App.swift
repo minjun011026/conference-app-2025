e70a0debaec55872bc0b4a72bdc1f5ed84947ae0
HyungwonJin <hwjin92@gmail.com>
2025-08-23 16:30:29 +0900

    chore: customize navigation bar font to use Chango-Regular

--- app-ios/App/DroidKaigi2025App.swift
+++ app-ios/App/DroidKaigi2025App.swift
@@ -10,6 +10,29 @@ import SwiftUI
 
 @main
 struct DroidKaigi2025App: App {
+    init() {
+        let nav = UINavigationBarAppearance()
+        nav.configureWithTransparentBackground()
+
+        let inlineBase = UIFont(name: "Chango-Regular", size: 20) ?? .systemFont(ofSize: 20, weight: .bold)
+        let largeBase  = UIFont(name: "Chango-Regular", size: 34) ?? .systemFont(ofSize: 34, weight: .bold)
+
+        let inlineFont = UIFontMetrics(forTextStyle: .headline).scaledFont(for: inlineBase)
+        let largeFont  = UIFontMetrics(forTextStyle: .largeTitle).scaledFont(for: largeBase)
+
+        nav.titleTextAttributes = [
+            .font: inlineFont,
+        ]
+        nav.largeTitleTextAttributes = [
+            .font: largeFont,
+        ]
+
+        let bar = UINavigationBar.appearance()
+        bar.standardAppearance = nav
+        bar.compactAppearance = nav
+        bar.scrollEdgeAppearance = nav
+    }
+
     var body: some Scene {
         WindowGroup {
             RootScreen()
