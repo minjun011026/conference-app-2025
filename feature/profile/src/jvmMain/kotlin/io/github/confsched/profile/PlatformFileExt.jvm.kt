6655b70fe093c189196ebc732bf024617946fe69
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-08-25 13:03:17 +0900

    refactor: remove persistent permission request since we now save images as ByteArray

--- feature/profile/src/jvmMain/kotlin/io/github/confsched/profile/PlatformFileExt.jvm.kt
+++
@@ -1,7 +0,0 @@
-package io.github.confsched.profile
-
-import io.github.vinceglb.filekit.PlatformFile
-
-actual fun PlatformFile.persistPermission() {
-    // this feature is expected to be used only in apps targeting Android
-}
