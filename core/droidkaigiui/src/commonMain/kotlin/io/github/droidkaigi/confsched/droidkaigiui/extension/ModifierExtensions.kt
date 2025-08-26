9f48fe576be516368c931e8b6a8491ba7d026d37
todayama_r <13657682+Corvus400@users.noreply.github.com>
2025-08-26 10:42:29 +0900

    :sparkles: Implemented Modifier extensions that enable Fling Scroll and CollapsingHeaderLayout integration on the JVM.

---
+++ core/droidkaigiui/src/commonMain/kotlin/io/github/droidkaigi/confsched/droidkaigiui/extension/ModifierExtensions.kt
@@ -0,0 +1,8 @@
+package io.github.droidkaigi.confsched.droidkaigiui.extension
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+expect fun Modifier.enableMouseDragScroll(lazyListState: LazyListState): Modifier
