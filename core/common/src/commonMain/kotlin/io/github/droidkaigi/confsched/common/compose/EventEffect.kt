99d0eba2f9826136cbd154bcb4d40f227c5ecc41
Haruto Muraki <114408959+Mgtantheta@users.noreply.github.com>
2025-08-27 00:36:23 +0900

    delete DataMutationContext

--- core/common/src/commonMain/kotlin/io/github/droidkaigi/confsched/common/compose/EventEffect.kt
+++ core/common/src/commonMain/kotlin/io/github/droidkaigi/confsched/common/compose/EventEffect.kt
@@ -2,9 +2,7 @@ package io.github.droidkaigi.confsched.common.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import io.github.droidkaigi.confsched.context.DataMutationContext
 import io.github.droidkaigi.confsched.context.PresenterContext
-import io.github.droidkaigi.confsched.context.useDataMutationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
@@ -23,15 +21,13 @@ fun <T> rememberEventFlow(): EventFlow<T> {
 context(_: PresenterContext)
 fun <EVENT> EventEffect(
     eventFlow: EventFlow<EVENT>,
-    block: suspend context (DataMutationContext) CoroutineScope.(event: EVENT) -> Unit,
+    block: suspend CoroutineScope.(event: EVENT) -> Unit,
 ) {
     SafeLaunchedEffect(eventFlow) {
-        useDataMutationContext {
-            supervisorScope {
-                eventFlow.collect { event ->
-                    launch {
-                        block(event)
-                    }
+        supervisorScope {
+            eventFlow.collect { event ->
+                launch {
+                    block(event)
                 }
             }
         }
