a18bb6cb556555e8d19ae59411b806ca389d3fb0
jun011026 <jun011026@gmail.com>
2025-08-28 10:46:19 +0900

    feat: tmp-ios

--- feature/profile/src/iosMain/kotlin/io/github/confsched/profile/InnerShadowModifier.kt
+++ feature/profile/src/iosMain/kotlin/io/github/confsched/profile/InnerShadowModifier.kt
@@ -1,9 +1,14 @@
 package io.github.confsched.profile
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.unit.Dp
+import org.jetbrains.skia.BlendMode
 
 actual fun Modifier.innerShadow(
     color: Color,
@@ -12,4 +17,31 @@ actual fun Modifier.innerShadow(
     offsetY: Dp,
     offsetX: Dp,
     spread: Dp
-) : Modifier = this
+) = drawWithContent {
+    drawContent()
+
+    drawIntoCanvas { canvas ->
+        val paint = Paint().apply {
+            this.color = color
+            this.asFrameworkPaint().also {
+                it.isAntiAlias = true
+                if (blur.toPx() > 0) {
+                    // Skia의 MaskFilter
+                    it.maskFilter = org.jetbrains.skia.MaskFilter.makeBlur(
+                        org.jetbrains.skia.FilterBlurMode.NORMAL,
+                        blur.toPx()
+                    )
+                }
+            }
+        }
+
+        val outline = shape.createOutline(size, layoutDirection, this)
+
+        paint.asFrameworkPaint().blendMode = BlendMode.DST_OUT
+
+        canvas.save()
+        canvas.translate(offsetX.toPx(), offsetY.toPx())
+        canvas.drawOutline(outline, paint)
+        canvas.restore()
+    }
+}
--- feature/profile/src/jvmMain/kotlin/io/github/confsched/profile/InnerShadowModifier.kt
+++ feature/profile/src/jvmMain/kotlin/io/github/confsched/profile/InnerShadowModifier.kt
@@ -1,8 +1,14 @@
 package io.github.confsched.profile
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.unit.Dp
 
 actual fun Modifier.innerShadow(
@@ -12,4 +18,30 @@ actual fun Modifier.innerShadow(
     offsetY: Dp,
     offsetX: Dp,
     spread: Dp
-) : Modifier = this
+) = this.drawWithContent {
+    drawContent()
+
+    drawIntoCanvas { canvas ->
+        val shadowSize = Size(size.width + spread.toPx(), size.height + spread.toPx())
+        val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
+
+        val paint = Paint().apply {
+            this.color = color
+        }
+
+        canvas.saveLayer(size.toRect(), paint)
+        canvas.drawOutline(shadowOutline, paint)
+
+        paint.asFrameworkPaint().apply {
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+            if (blur.toPx() > 0) {
+                maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+            }
+        }
+
+        paint.color = Color.Black
+        canvas.translate(offsetX.toPx(), offsetY.toPx())
+        canvas.drawOutline(shadowOutline, paint)
+        canvas.restore()
+    }
+}
