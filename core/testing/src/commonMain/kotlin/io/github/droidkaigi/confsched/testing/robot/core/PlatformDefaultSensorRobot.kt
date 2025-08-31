a2b1548b8e50fa318e683f2af6a17189b656a584
todayama_r <13657682+Corvus400@users.noreply.github.com>
2025-09-01 06:17:01 +0900

    :wrench: I forgot to add Inject, so I added it.

--- core/testing/src/commonMain/kotlin/io/github/droidkaigi/confsched/testing/robot/core/PlatformDefaultSensorRobot.kt
+++ core/testing/src/commonMain/kotlin/io/github/droidkaigi/confsched/testing/robot/core/PlatformDefaultSensorRobot.kt
@@ -1,5 +1,7 @@
 package io.github.droidkaigi.confsched.testing.robot.core
 
+import dev.zacsweers.metro.Inject
+
 interface SensorRobot {
     fun setupMockSensors(sensorTypes: List<SensorType>)
     fun cleanUpSensors()
@@ -13,6 +15,7 @@ interface SensorRobot {
  * - This sensor robot only supports Euler angles.
  * - TODO If necessary, add processing to handle quaternions.
  */
+@Inject
 expect class PlatformDefaultSensorRobot : SensorRobot {
     override fun setupMockSensors(sensorTypes: List<SensorType>)
     override fun cleanUpSensors()
