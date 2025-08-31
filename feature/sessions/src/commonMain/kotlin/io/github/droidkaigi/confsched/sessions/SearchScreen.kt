9975076c94c4bd60b7d6747f32f41340c5f267ab
urotarou <devak9kflab@gmail.com>
2025-08-31 23:38:36 +0900

    Create a Preview of the SearchScreen

--- feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/SearchScreen.kt
+++ feature/sessions/src/commonMain/kotlin/io/github/droidkaigi/confsched/sessions/SearchScreen.kt
@@ -9,10 +9,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import io.github.droidkaigi.confsched.droidkaigiui.session.TimetableList
+import io.github.droidkaigi.confsched.model.core.DroidKaigi2025Day
+import io.github.droidkaigi.confsched.model.sessions.TimetableItem
 import io.github.droidkaigi.confsched.model.sessions.TimetableItemId
+import io.github.droidkaigi.confsched.model.sessions.fake
+import io.github.droidkaigi.confsched.sessions.SearchScreenUiState.TimeSlot
 import io.github.droidkaigi.confsched.sessions.components.SearchFilterRow
 import io.github.droidkaigi.confsched.sessions.components.SearchNotFoundContent
 import io.github.droidkaigi.confsched.sessions.components.SearchTopBar
+import kotlinx.collections.immutable.PersistentMap
+import kotlinx.collections.immutable.persistentMapOf
+import kotlinx.datetime.LocalTime
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.ui.tooling.preview.PreviewParameter
+import org.jetbrains.compose.ui.tooling.preview.PreviewParameterProvider
 
 @Composable
 fun SearchScreen(
@@ -68,5 +78,44 @@ fun SearchScreen(
                 }
             }
         }
+    }
+}
+
+private class SearchScreenPreviewParameterProvider : PreviewParameterProvider<PersistentMap<TimeSlot, List<TimetableItem>>> {
+    override val values: Sequence<PersistentMap<TimeSlot, List<TimetableItem>>> = sequenceOf(
+        persistentMapOf(
+            TimeSlot(
+                startTime = LocalTime.parse("11:20"),
+                endTime = LocalTime.parse("12:00"),
+            ) to listOf(
+                TimetableItem.Session.fake(),
+            ),
+        ),
+        persistentMapOf(),
+    )
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview(
+    @PreviewParameter(SearchScreenPreviewParameterProvider::class) groupedSessions: PersistentMap<TimeSlot, List<TimetableItem>>,
+) {
+    KaigiPreviewContainer {
+        SearchScreen(
+            uiState = SearchScreenUiState(
+                searchQuery = "DroidKaigi",
+                groupedSessions = groupedSessions,
+                availableFilters = SearchScreenUiState.Filters(
+                    availableDays = listOf(
+                        DroidKaigi2025Day.ConferenceDay1,
+                        DroidKaigi2025Day.ConferenceDay2,
+                    ),
+                ),
+                hasSearchCriteria = true,
+            ),
+            onBackClick = {},
+            onTimetableItemClick = {},
+            onEvent = {},
+        )
     }
 }
